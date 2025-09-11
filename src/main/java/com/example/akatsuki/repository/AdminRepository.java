783c088c33ef80fb1c44ff810413c9b54c644b0a
Swati <swatisha@gmail.com>
2025-09-11 18:57:05 +0530

    Third commit

--- src/main/java/com/example/akatsuki/repository/AdminRepository.java
+++ src/main/java/com/example/akatsuki/repository/AdminRepository.java
@@ -3,6 +3,6 @@ package com.example.akatsuki.repository;
 import com.example.akatsuki.model.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AdminRepository extends JpaRepository<Admin, String> {
-    Admin findByEmail(String email);
+public interface AdminRepository extends JpaRepository<Admin, Integer> {
+    Admin findByAdminId(Integer adminId);
 }
--- src/main/java/com/example/akatsuki/repository/UserRepository.java
+++ src/main/java/com/example/akatsuki/repository/UserRepository.java
@@ -2,6 +2,6 @@ package com.example.akatsuki.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.akatsuki.model.User;
-public interface UserRepository extends JpaRepository<User, String> {
-    User findByEmail(String email);
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByUserId(Integer userId);
 }
